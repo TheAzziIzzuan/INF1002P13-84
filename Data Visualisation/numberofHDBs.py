ab5699539459d6855c61e73dafe992f2fa82844a
nadasgithub <146849880+nadasgithub@users.noreply.github.com>
2023-10-06 09:16:56 +0800

    Implement Data Cleaning to show total units in each area

--- Data Visualisation/numberofHDBs.py
+++ Data Visualisation/numberofHDBs.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+#pivot table the big data set to show amount of units in each area
+df = pd.read_csv("project.py\HDBPropertyInformation.csv")
+pivot = df.pivot_table(index =['bldg_contract_town'],  
+                       values =['total_dwelling_units'], aggfunc ='sum') 
+
 #plot data frame 
 df = pd.read_csv("project.py\SumDwellingUnit(2013-2023).csv")
 bargraph = df.plot.bar(x='Row Labels')
