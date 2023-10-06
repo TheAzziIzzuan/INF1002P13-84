ac562a3255ff28064e36f7ce02172f3f7b774249
Azzi Izzuan <theazziizzuan@gmail.com>
2023-10-06 21:29:14 +0800

    Added Remaining Years Impact on Sale for all room type

---
+++ Data Visualisation/RemainingYearsImpactOnSale.py
@@ -0,0 +1,36 @@
+import datetime
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def RemainingYearImpactonSale(room_type):
+    data = pd.read_csv('..\datasets\Resale_Flat_Prices_Jan_2013_to_Sep_2023.csv')
+
+    two_room_data = data[data["flat_type"] == room_type].copy()
+
+    # gets data of prices and lease commence date
+    # two_room_prices = two_room_data["resale_price"]
+    # lease_commence_date = two_room_data["lease_commence_date"]
+
+    # get the current year
+    current_year = datetime.datetime.now().year
+    # get remaining lease year by subtracting 99 - (current year - lease commence date)
+    two_room_data.loc[:, "remaining_years"] = 99 - (current_year - two_room_data["lease_commence_date"].astype(int))
+
+    # plot scatter plot using scatter()
+    plt.figure(figsize=(10, 6))
+    plt.scatter(two_room_data["remaining_years"], two_room_data["resale_price"], alpha=0.5)
+    plt.title(f"{room_type} Resale Flat Prices.")
+    plt.xlabel("Age Left")
+    plt.ylabel("Resale Price")
+    plt.grid(True)
+    plt.show()
+
+
+# Get Remaining Year Impact on Sale for all type of room
+RemainingYearImpactonSale("2 ROOM")
+RemainingYearImpactonSale("3 ROOM")
+RemainingYearImpactonSale("4 ROOM")
+RemainingYearImpactonSale("5 ROOM")
+RemainingYearImpactonSale("EXECUTIVE")
