bec4f9278d5e45a191eda0a2eae808935594e2a3
Jonderic Ualath <jonderic@live.com>
2023-10-06 16:19:03 +0800

    Update resalePricesByTown.py
    Removed unused extra lines.

--- Data Visualisation/resalePricesByTown.py
+++ Data Visualisation/resalePricesByTown.py
@@ -3,13 +3,6 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np
 
-# #test reading of CSV file
-# with open('datasets/Resale_Flat_Prices_Jan_2015_to_Dec_2016.csv') as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     line_count = 0
-#     for row in csv_reader:
-#         print(row)
-
 # Initialize the lists for X and Y 
 data = pd.read_csv('datasets\Resale_Flat_Prices_Jan_2013_to_Sep_2023.csv') 
   
@@ -21,8 +14,8 @@ Y = list(df.iloc[:, 9])
 # Plot the data using bar() method 
 plt.barh(X, Y, color='g') 
 plt.title("Resale Price by Towns") 
-plt.xlabel("Town") 
-plt.ylabel("Resale Price") 
+plt.xlabel("Resale Price") 
+plt.ylabel("Towns") 
   
 # Show the plot 
 plt.show() 
