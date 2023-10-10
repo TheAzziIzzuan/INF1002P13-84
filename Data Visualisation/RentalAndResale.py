92a98d848a2424b029a491dbb2e88f28a6eb2a03
Jonderic Ualath <jonderic@live.com>
2023-10-10 21:05:29 +0800

    Update RentalAndResale.py
    Tkinter integration in progress.

--- Data Visualisation/RentalAndResale.py
+++ Data Visualisation/RentalAndResale.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from tkinter import *
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg 
+
+########################### FUNCTIONS TO GENERATE GRAPHS ##################################
 
 def ResaleAndRentalApplications():
     #read in CSV
@@ -28,8 +32,6 @@ def ResaleAndRentalApplications():
     plt.legend(loc="lower right")
     plt.show()
 
-ResaleAndRentalApplications()
-
 def RentalbyFlatType():
     #read in CSV
     df = pd.read_csv("datasets\RentalByFlatType.csv")
@@ -48,4 +50,30 @@ def RentalbyFlatType():
     plt.suptitle("Number of Approved Renting Out Applications by Flat Type")
     plt.show()
 
-RentalbyFlatType()
+############################ TKINTER WINDOW SECTION ###############################################
+
+#Initialize window
+root = Tk()
+fig, ax = plt.subplots()
+#Set size of window
+root.geometry("640x480")
+#Title of window
+root.title("Rental and Resale")
+frame = Frame(root)
+
+#Upper frame showing buttons
+controls_frame = LabelFrame(root, text='List of Graphs', background='light grey', height=5)
+controls_frame.pack(fill='both', expand='0', side=TOP, padx=20, pady=10)
+redbutton = Button(controls_frame, text = 'Renting out of Applications by Flat Type', fg ='red',command=RentalbyFlatType) 
+redbutton.pack( side = LEFT)
+greenbutton = Button(controls_frame, text = 'Resale vs Rental Applications Registered', fg='brown',command=ResaleAndRentalApplications) 
+greenbutton.pack( side = RIGHT )
+
+#lower box showing plot that changes according to button clicked
+plotting_frame = LabelFrame(root, text='Graph', bg='white', width=300, height=440, bd=5, relief=SUNKEN)
+canvas = FigureCanvasTkAgg(fig, root)
+canvas.draw()
+canvas.get_tk_widget().pack(fill=Tk.BOTH, expand=True)
+plotting_frame.pack(fill='both', expand='yes', side=BOTTOM, padx=20)
+
+root.mainloop() 
