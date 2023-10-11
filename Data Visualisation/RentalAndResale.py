bb49cf1c0dcfa26a05fa1a9e49aca62ca3852df1
Jonderic Ualath <jonderic@live.com>
2023-10-11 13:24:38 +0800

    Update RentalAndResale.py
    Pseudo-working code of interchangeable graphs on single canvas with clear canvas button.

--- Data Visualisation/RentalAndResale.py
+++ Data Visualisation/RentalAndResale.py
@@ -2,35 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from tkinter import *
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg 
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 ########################### FUNCTIONS TO GENERATE GRAPHS ##################################
-
-def ResaleAndRentalApplications():
-    #read in CSV
-    df = pd.read_csv("datasets\ResaleAndRentalApplications.csv")
-
-    #assign values to variables
-    resale = df['resale'].tolist()
-    rental = df['rental'].tolist()
-    year = df['year'].tolist()
-
-    #generate resale bars
-    resaleBars = plt.barh(year,resale,label="Resale")
-    #generate rental bars
-    rentalBars = plt.barh(year,rental,label="Rental")
-    #label bars with values
-    plt.bar_label(resaleBars,labels=resale,label_type="center")
-    plt.bar_label(rentalBars,labels=rental,label_type="center")
-
-    #visible grid lines
-    plt.grid(color='#95a5a6', linestyle='--', linewidth=1, axis='x', alpha=0.7)
-    plt.ylabel("Years")
-    plt.xlabel("Applications Registered")
-    plt.title("Applications Registered For Resale and Rental Flats")
-    plt.yticks(year)
-    plt.legend(loc="lower right")
-    plt.show()
 
 def RentalbyFlatType():
     #read in CSV
@@ -48,32 +23,69 @@ def RentalbyFlatType():
     fig.subplots_adjust(wspace=.2)
     fig.delaxes(axes[2,3])
     plt.suptitle("Number of Approved Renting Out Applications by Flat Type")
-    plt.show()
+    return fig
+
+def ResaleAndRentalApplications():
+    #read in CSV
+    df = pd.read_csv("datasets\ResaleAndRentalApplications.csv")
+
+    #assign values to variables
+    resale = df['resale'].tolist()
+    rental = df['rental'].tolist()
+    year = df['year'].tolist()
+
+    fig, ax = plt.subplots()
+    resaleBars = ax.barh(year, resale,label="Resale")
+    rentalBars = ax.barh(year, rental,label="Rental")
+    plt.bar_label(resaleBars,labels=resale,label_type="center")
+    plt.bar_label(rentalBars,labels=rental,label_type="center")
+
+    #visible grid lines
+    plt.grid(color='#95a5a6', linestyle='--', linewidth=1, axis='x', alpha=0.7)
+    plt.ylabel("Years")
+    plt.xlabel("Applications Registered")
+    plt.title("Applications Registered For Resale and Rental Flats")
+    plt.yticks(year)
+    plt.legend(loc="lower right")
+    
+    return fig
 
 ############################ TKINTER WINDOW SECTION ###############################################
 
 #Initialize window
-root = Tk()
-fig, ax = plt.subplots()
+root = tk.Tk()
 #Set size of window
 root.geometry("640x480")
 #Title of window
 root.title("Rental and Resale")
 frame = Frame(root)
 
+canvas = None
+
+def update_canvas(plot_function):
+    global canvas
+    # Generate and display the selected plot
+    fig = plot_function()
+    canvas = FigureCanvasTkAgg(fig, master=frame)
+    canvas_widget = canvas.get_tk_widget()
+    canvas_widget.pack(fill='both', expand=True)
+
+def clear_canvas():
+    global canvas  # Use the global canvas variable
+    if canvas is not None:
+        canvas.get_tk_widget().destroy()
+        canvas = None
+
 #Upper frame showing buttons
 controls_frame = LabelFrame(root, text='List of Graphs', background='light grey', height=5)
 controls_frame.pack(fill='both', expand='0', side=TOP, padx=20, pady=10)
-redbutton = Button(controls_frame, text = 'Renting out of Applications by Flat Type', fg ='red',command=RentalbyFlatType) 
-redbutton.pack( side = LEFT)
-greenbutton = Button(controls_frame, text = 'Resale vs Rental Applications Registered', fg='brown',command=ResaleAndRentalApplications) 
-greenbutton.pack( side = RIGHT )
+RBFTbutton = Button(controls_frame, text = 'Renting out of Applications by Flat Type', command=lambda: update_canvas(RentalbyFlatType))
+RBFTbutton.pack( side = LEFT)
+RVRAbutton = Button(controls_frame, text = 'Resale vs Rental Applications Registered', command=lambda: update_canvas(ResaleAndRentalApplications))
+RVRAbutton.pack( side = RIGHT )
+clear_button = tk.Button(controls_frame, text="Clear Graph", command=clear_canvas)
+clear_button.pack(side = TOP)
 
-#lower box showing plot that changes according to button clicked
-plotting_frame = LabelFrame(root, text='Graph', bg='white', width=300, height=440, bd=5, relief=SUNKEN)
-canvas = FigureCanvasTkAgg(fig, root)
-canvas.draw()
-canvas.get_tk_widget().pack(fill=Tk.BOTH, expand=True)
-plotting_frame.pack(fill='both', expand='yes', side=BOTTOM, padx=20)
+frame.pack(fill='both', expand=True)
 
-root.mainloop() 
+root.mainloop()
