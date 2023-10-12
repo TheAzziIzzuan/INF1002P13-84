0a753785f61f5af070ef6c07637822949030259f
nadasgithub <146849880+nadasgithub@users.noreply.github.com>
2023-10-12 20:11:14 +0800

    prepare dataset for scattermap
    removed columns except resale price, coordinates of HDB and nearest mrt

---
+++ Data Cleaning/priceandDistclean.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+
+with open("project/datasets/hdb_latest.csv", "r") as source: 
+    reader = csv.reader(source) 
+      
+    with open("priceanddist.csv", "w") as result: 
+        writer = csv.writer(result) 
+        for r in reader: 
+            # copies only resale price and distance
+            writer.writerow((r[6], r[17]))
