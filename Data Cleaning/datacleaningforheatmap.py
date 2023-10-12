4556952cfdd9dd96812d7b5c9eb0098c4e70ad61
nadasgithub <146849880+nadasgithub@users.noreply.github.com>
2023-10-12 20:07:38 +0800

    Update datacleaningforheatmap.py

--- Data Cleaning/datacleaningforheatmap.py
+++ Data Cleaning/datacleaningforheatmap.py
@@ -1,6 +1,6 @@
 import csv  
  
-with open("dataset\hdb-property-coords.csv", "r") as source: 
+with open("datasets\hdb-property-coords.csv", "r") as source: 
     reader = csv.reader(source) 
       
     with open("output.csv", "w") as result: 
