5a054360314fd0392a23a3314fdd2cf478335819
Azzi Izzuan <theazziizzuan@gmail.com>
2023-10-12 19:38:59 +0800

    displayed scatter plot on canvas, fixed the export button

--- Data Visualisation/remainingYearsImpactOnSale.py
+++ Data Visualisation/remainingYearsImpactOnSale.py
@@ -1,18 +1,27 @@
 import datetime
 import tkinter
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import tkinter as tk
 from tkinter import ttk
 import customtkinter
 from tkinter import filedialog
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+canvas = None
+currentroomtype = None
 
 
-def df_to_csv(room_data, filename_prefix):
-    filename = f"{filename_prefix}.csv"
-    df = pd.DataFrame(room_data)
-    df.to_csv(filename, index=False)
+# export function
+def exportcsv(room_type):
+    if currentroomtype is not None:  # if the global current room type is not empty
+        data = remainingYearImpactonSale(room_type)  # get its data from remainingYearsImpactonSale Function
+        filename = f"List of {room_type}.csv"  # get name of room_type user selected
+        df = pd.DataFrame(data)  # select the data that has the room_type
+        df.to_csv(filename, index=False)
+    else:
+        tkinter.messagebox.showinfo("ERROR!",
+                                    "No Room Type is selected!")  # if global current room type is empty it will return a error pop up
 
 
 # gets data from csv and split them into individual data
@@ -31,18 +40,21 @@ def remainingYearImpactonSale(room_type):
     room_data.loc[:, "remaining_years"] = 99 - (current_year - room_data["lease_commence_date"].astype(int))
 
     # plot scatter plot using scatter()
-    plt.figure(figsize=(10, 6))
-    plt.scatter(room_data["remaining_years"], room_data["resale_price"], alpha=0.5)
-    plt.title(f"{room_type} Resale Flat Prices.")
-    plt.xlabel("Age Left")
-    plt.ylabel("Resale Price")
-    plt.grid(True)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.scatter(room_data["remaining_years"], room_data["resale_price"], alpha=0.5)
+    ax.set_title(f"{room_type} Resale Flat Prices.")
+    ax.set_xlabel("Age Left")
+    ax.set_ylabel("Resale Price")
+    ax.grid(True)
 
-    exportbutton = tkinter.Button(text="Export", command=lambda: df_to_csv(room_data, room_type))
-    exportbutton.pack()
+    global canvas  # declares a global canvas
+    if canvas is not None:  # if there is a graph displayed on the the canvas
+        canvas.get_tk_widget().destroy()  # it will destroy before displaying another graph
 
-    plt.show()
-
+    canvas = FigureCanvasTkAgg(fig, master=gui)
+    canvas.get_tk_widget().pack()
+    canvas.draw()
+    return room_data
 
 
 # Get Remaining Year Impact on Sale for all type of room
@@ -55,11 +67,15 @@ def remainingYearImpactonSale(room_type):
 
 # this function is when a button is click,it executes the command that calls the graph which is remainingYearImpactonSale(room_type)
 def click_button(room_type):
+    global currentroomtype
+    currentroomtype = room_type
     remainingYearImpactonSale(room_type)
 
 
-customtkinter.set_appearance_mode("system")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+#####################tkinter section########################
+# this function uses customtkinter to set the colours of background and buttons.
+customtkinter.set_appearance_mode("light")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
 gui = customtkinter.CTk()
 
@@ -71,23 +87,27 @@ room_types = ["2 ROOM", "3 ROOM", "4 ROOM", "5 ROOM", "EXECUTIVE"]
 
 # text section
 message = "This page shows if the remaining years of HDB(the Age) affects the resale price"
-label = customtkinter.CTkLabel(gui, text=message, font=("Arial", 30))
-label.pack(anchor="center", expand=True)
+label = customtkinter.CTkLabel(gui, text=message, font=("Arial", 25))
+label.pack(anchor="center")
 
 # buttons section
+# from two,three,four,five and executive buttons, the buttons will call the function of showing the graph depending on the room type that user selected
 TWO_ROOM_BUTTON = customtkinter.CTkButton(gui, text=room_types[0], command=lambda: click_button(room_types[0]))
-TWO_ROOM_BUTTON.place(relx=0.1, rely=0.8, anchor=customtkinter.CENTER)
+TWO_ROOM_BUTTON.place(relx=0.1, rely=0.92, anchor=customtkinter.CENTER)
 
 THREE_ROOM_BUTTON = customtkinter.CTkButton(gui, text=room_types[1], command=lambda: click_button(room_types[1]))
-THREE_ROOM_BUTTON.place(relx=0.3, rely=0.8, anchor=customtkinter.CENTER)
+THREE_ROOM_BUTTON.place(relx=0.3, rely=0.92, anchor=customtkinter.CENTER)
 
 FOUR_ROOM_BUTTON = customtkinter.CTkButton(gui, text=room_types[2], command=lambda: click_button(room_types[2]))
-FOUR_ROOM_BUTTON.place(relx=0.5, rely=0.8, anchor=customtkinter.CENTER)
+FOUR_ROOM_BUTTON.place(relx=0.5, rely=0.92, anchor=customtkinter.CENTER)
 
 FIVE_ROOM_BUTTON = customtkinter.CTkButton(gui, text=room_types[3], command=lambda: click_button(room_types[3]))
-FIVE_ROOM_BUTTON.place(relx=0.7, rely=0.8, anchor=customtkinter.CENTER)
+FIVE_ROOM_BUTTON.place(relx=0.7, rely=0.92, anchor=customtkinter.CENTER)
 
 EXECUTIVE_ROOM_BUTTON = customtkinter.CTkButton(gui, text=room_types[4], command=lambda: click_button(room_types[4]))
-EXECUTIVE_ROOM_BUTTON.place(relx=0.9, rely=0.8, anchor=customtkinter.CENTER)
+EXECUTIVE_ROOM_BUTTON.place(relx=0.9, rely=0.92, anchor=customtkinter.CENTER)
+
+EXPORT = customtkinter.CTkButton(gui, text="EXPORT TO CSV", command=lambda: exportcsv(currentroomtype))
+EXPORT.place(relx=0.5, rely=0.97, anchor=customtkinter.CENTER)
 
 gui.mainloop()
