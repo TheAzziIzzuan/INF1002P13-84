f4a3b42ee8daaaac9f5603fda075c62cea948a2d
Azzi Izzuan <theazziizzuan@gmail.com>
2023-10-12 19:55:07 +0800

    added LoanCalculator.py

---
+++ Data Visualisation/LoanCalculator.py
@@ -0,0 +1,62 @@
+import sys
+
+def fifteen_years_loan(income):
+    if income <= 1000:
+        estimated_loan_amount = "$62,000"
+    elif income <= 2000:
+        estimated_loan_amount = "$126,000"
+    elif income <= 3000:
+        estimated_loan_amount = "$189,000"
+    elif income <= 4000:
+        estimated_loan_amount = "$253,000"
+    elif income <= 5000:
+        estimated_loan_amount = "$316,000"
+    elif income <= 6000:
+        estimated_loan_amount = "$379,000"
+    elif income <= 7000:
+        estimated_loan_amount = "$442,000"
+    elif income <= 8000:
+        estimated_loan_amount = "$506,000"
+    elif income <= 9000:
+        estimated_loan_amount = "$569,000"
+    elif income <= 10000:
+        estimated_loan_amount = "$632,000"
+    elif income <= 11000:
+        estimated_loan_amount = "$695,000"
+    elif income <= 12000:
+        estimated_loan_amount = "$759,000"
+    elif income <= 13000:
+        estimated_loan_amount = "$822,000"
+    else:
+        estimated_loan_amount = "$885,000"
+
+def twenty_five_years_loan(income):
+    if income <= 1000:
+        estimated_loan_amount = "$62,000"
+    elif income <= 2000:
+        estimated_loan_amount = "$126,000"
+    elif income <= 3000:
+        estimated_loan_amount = "$189,000"
+    elif income <= 4000:
+        estimated_loan_amount = "$253,000"
+    elif income <= 5000:
+        estimated_loan_amount = "$316,000"
+    elif income <= 6000:
+        estimated_loan_amount = "$379,000"
+    elif income <= 7000:
+        estimated_loan_amount = "$442,000"
+    elif income <= 8000:
+        estimated_loan_amount = "$506,000"
+    elif income <= 9000:
+        estimated_loan_amount = "$569,000"
+    elif income <= 10000:
+        estimated_loan_amount = "$632,000"
+    elif income <= 11000:
+        estimated_loan_amount = "$695,000"
+    elif income <= 12000:
+        estimated_loan_amount = "$759,000"
+    elif income <= 13000:
+        estimated_loan_amount = "$822,000"
+    else:
+        estimated_loan_amount = "$885,000"
+
